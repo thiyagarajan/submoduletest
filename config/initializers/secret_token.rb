5ce067d6c7e1c672ea44eed7345b1bc2e1667a9b
thiyagarajan <thiyagarajannv@yahoo.com>
2012-02-03 11:21:23 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vtr::Application.config.secret_token = '65be8245944ba4522e02423e73bae57e7b534ac8216b2200fd35594e5b286084ed46830513c95b33b6554fd027f8e39388524ee9123f44e3460e6342ffee4330'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Vtr::Application.config.session_store :cookie_store, key: '_vtr_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Vtr::Application.config.session_store :active_record_store
